c4d96275ce044edcd3c55dc52e82b6d224ecb302
bataspen <38957961+bataspen@users.noreply.github.com>
2018-05-07 17:23:33 -0400

    Add files via upload

---
+++ ESO Prices.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  2 15:28:28 2018
+
+Useful ESO prices
+
+@author: Spencer
+"""
+
+reagentPrices = {'Blessed Thistle':250,'Blue Entoloma':100,'Bugloss':100,'Beetle Scuttle':150,'Butterfly Wing':75,"Fleshfly Larva":25,
+              'Mudcrab Chitin':75,'Scrib Jelly':75,'Spider Egg':50,'Torchbug Thorax':100,'Wormwood':75,'White Cap':50,
+              'Water Hyacinth':125,'Stinkhorn':75,'Nirnroot':60,'Mountain Flower':60,'Lady\'s Smock':200,'Imp Stool':100,
+              'Dragonthorn':60,'Columbine':250,'Corn Flower':300,'Emetic Russula':60,'Luminous Russula':75,'Namira\'s Rot':250,
+              'Violet Coprinus':90,'Nightshade':100}
---
+++ apiclass.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 15:45:14 2018
+
+beginning of fun lol api coding
+
+@author: Spencer
+"""
+
+#import necessary libraries
+import json
+import urllib
+
+"""
+example call
+print(json.loads(urllib.request.urlopen(
+'https://na1.api.riotgames.com/lol/static-data/v3/champions?api_key=RGAPI-830909d0-99e2-4394-afdf-3c074eee9ae7').read()))
+"""
+
+class 
